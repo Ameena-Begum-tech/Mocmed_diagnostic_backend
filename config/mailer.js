a2a7670587bd93dde1f9766033488a156ae6d453
Ameena Begum <ameenabegum140@gmail.com>
2026-02-18 16:22:05 +0530

    Update mailer configuration for Gmail transport

--- config/mailer.js
+++ config/mailer.js
@@ -2,10 +2,20 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  tls: {
+    rejectUnauthorized: false,
+  },
+
+  family: 4, // 🚨 THIS FIXES RENDER (forces IPv4)
 });
 
 module.exports = transporter;
