5a2ec06a9a2905ca7a99d5df8b78621e421045c0
Ameena Begum <ameenabegum140@gmail.com>
2026-02-22 07:44:59 +0530

    Update mailer.js

--- config/mailer.js
+++ config/mailer.js
@@ -1,26 +1,11 @@
-const { Resend } = require("resend");
+const nodemailer = require("nodemailer");
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
-const sendOTP = async (email, otp) => {
-  try {
-    await resend.emails.send({
-      from: "Mocmed Diagnostics <onboarding@resend.dev>",
-      to: email,
-      subject: "Verify your Mocmed account",
-      html: `
-        <h2>Mocmed Account Verification</h2>
-        <p>Your OTP:</p>
-        <h1>${otp}</h1>
-        <p>Valid for 5 minutes</p>
-      `,
-    });
-
-    console.log("OTP Email Sent");
-  } catch (error) {
-    console.log("Email error:", error);
-    throw new Error("Email sending failed");
-  }
-};
-
-module.exports = sendOTP;
+module.exports = transporter;
