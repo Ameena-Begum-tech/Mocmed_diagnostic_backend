ef5bca90b212327c2b9504413ec214db8ddcc358
Ameena Begum <ameenabegum140@gmail.com>
2026-02-22 15:08:19 +0530

    Update mailer.js

--- config/mailer.js
+++ config/mailer.js
@@ -1,32 +1,20 @@
-// config/mailer.js
 // Language: Node.js (JavaScript)
 
 const nodemailer = require("nodemailer");
-const dns = require("dns");
-
-// ⭐ Force Node to prefer IPv4
-dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
 });
 
-// ⭐ Debug SMTP connection
-transporter.verify((error, success) => {
-  if (error) {
-    console.log("SMTP ERROR:", error.message);
-  } else {
-    console.log("SMTP READY");
-  }
+transporter.verify((err) => {
+  if (err) console.log("SMTP ERROR:", err.message);
+  else console.log("SMTP READY (BREVO)");
 });
 
 module.exports = transporter;
