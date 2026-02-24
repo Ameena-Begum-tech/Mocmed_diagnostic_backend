60ccc773433e579a4b11b3a1511fa1c49abb39d4
Ameena Begum <ameenabegum140@gmail.com>
2026-02-24 20:50:54 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,6 +14,7 @@ const {
 
 const { protect, authorize } = require("../middleware/authMiddleWare");
 const { viewReport } = require("../controllers/reportController");
+const { resendVerifyOtp } = require("../controllers/authController");
 
 // Routes
 router.post("/register", registerUser);
@@ -22,6 +23,7 @@ router.post("/verify-otp", verifyOtp);
 router.post("/forgot-password", forgotPassword);
 router.post("/verify-reset-otp", verifyResetOtp);
 router.post("/reset-password", resetPassword);
+router.post("/resend-otp", resendVerifyOtp);
 
 router.get("/patients", protect, authorize("SUPERADMIN"), getPatients);
 router.get("/view/:reportId", protect, viewReport);
